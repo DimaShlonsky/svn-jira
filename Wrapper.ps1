4149f59ebb6f0dea8d16aba20956ddd8bcfdd8cc
Dima Shlonsky <dima@toptix.co.il>
2018-01-16 18:29:56 +1100

    Removed junk

--- Wrapper.ps1
+++
@@ -1,3 +0,0 @@
-$args | Out-File -FilePath $PSScriptRoot\log.txt -Append
-Set-PSDebug -Trace 2
-&"${PSScriptRoot}post-commit-hook.ps1 -messageFile $args[2] -revisionNumber $args[3] -workingDir $args[5] | Out-File -FilePath $PSScriptRoot\log.txt -Append"

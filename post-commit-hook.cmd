6649ee9f9c07b0f9acade8198d31ef595108d00a
Dima Shlonsky <dima@toptix.co.il>
2018-01-25 15:56:37 +1100

    this shouldn't be here

--- post-commit-hook.cmd
+++
@@ -1,2 +0,0 @@
-powershell.exe -ExecutionPolicy Unrestricted -File "%~dp0\commit-hook.ps1" -phase post -pathsFile "%1" -depth %2 -messageFile "%3" -workingDir "%4" -Debug >con 2>&1
-pause < con > con

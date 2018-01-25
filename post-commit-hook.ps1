0c772e1d8afb4426682b2d3a5c04cbf962c9b40d
Dima Shlonsky <dima@toptix.co.il>
2018-01-25 15:57:29 +1100

    Fixed no error output from post-commit-hook to svn

--- post-commit-hook.ps1
+++ post-commit-hook.ps1
@@ -6,5 +6,7 @@ param(
     [string]$errorFile,
     [string]$workingDir
 )
+$ErrorActionPreference ="Stop"
+$DebugPreference = "Continue"
 $command = "`"${PSScriptRoot}\commit-hook.ps1`" -phase post -pathsFile $pathsFile -depth $depth -messageFile $messageFile -revisionNumber $revisionNumber -errorFile $errorFile  -workingDir $workingDir";
 Invoke-Expression "& $command "
